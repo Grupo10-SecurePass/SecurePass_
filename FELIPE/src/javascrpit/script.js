06b009d4674e17329100bf2f4ac226b531fda2b0
FMAmorim <felipe.amorim@sptech.school>
2024-08-24 13:51:32 -0300

    Arrumando o Header

--- FELIPE/src/javascrpit/script.js
+++ FELIPE/src/javascrpit/script.js
@@ -1,6 +1,42 @@
-$(document).ready(function(){
-    $('#mobile_btn').on('click', function(){
-        $('#mobile_menu').toogleClass('active')
-        $('#mobile_btn').find('i').toogleClass('fa-x')
-    })
-})
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5 ease forwards 0.3s`);
+        })
+    }
+
+     handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+     }
+
+    addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+}
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init();
