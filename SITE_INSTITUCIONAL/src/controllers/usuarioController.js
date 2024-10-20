53861938d6456c66263c7183413178d29cfffc98
AyrtonCasa <ayrton.casa@sptech.school>
2024-10-20 15:57:03 -0300

    idUsuário consertado no controller usuario

--- SITE_INSTITUCIONAL/src/controllers/usuarioController.js
+++ SITE_INSTITUCIONAL/src/controllers/usuarioController.js
@@ -24,7 +24,7 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
                         res.json({
-                            id: resultadoAutenticar[0].idusuario,
+                            idUsuario: resultadoAutenticar[0].idUsuario,
                             email: resultadoAutenticar[0].email,
                             nome: resultadoAutenticar[0].nome,
                             cargo: resultadoAutenticar[0].cargo,
