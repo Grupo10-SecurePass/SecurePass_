f9c66f7a577e415832257142edb39a9e946b8c47
Ayrton Casa <ayrton.casa@sptech.school>
2024-11-30 13:37:32 -0300

    Pronto para ambiente de produção

--- SITE_INSTITUCIONAL/app.js
+++ SITE_INSTITUCIONAL/app.js
@@ -1,5 +1,5 @@
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
+var ambiente_processo = 'producao';
+// var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
